ad5527f04b5ea5bf5e308dacf875fdc32541ff08
phamduynghia-it <162310524+phamduynghia-it@users.noreply.github.com>
2026-07-08 16:36:04 +0700

    Deploy greeting card with dynamic assets and custom workflow

---
+++ config.js
@@ -0,0 +1,21 @@
+const CONFIG = {
+    // Tiêu đề của trang và thiệp
+    title: `Love You 💖`,
+
+    // Link nhạc nền
+    music: "./assets/user_music.mp3",
+    
+    // Lời nhắn hiển thị trong phần thiệp
+    popupMessage: `Gửi đến anh chồng của toaiii!!!
+Mong chồng sẽ iu em thật nhiều , trân trọng em & mãi mãi bên cạnh nhau nhá, đừng làm vợ buồn nữa, vợ & con luôn iu thương chồng nhiều , ở bên cạnh động viên & luôn khỏe mạnh ck nha, thật hạnh phúc vì là vợ của chồng !!!
+Yêu chồng nhiều ♥️`,
+    
+    // Các câu chữ bay lên xung quanh trái tim 3D
+    messages: ["Chúc mừng 10 năm của 2 ta","11/6/2016--->>2026","Huy My Nhi ♥️","Luôn iu thương vợ nhé!!! 🫶🫶","Chồng là người tuyệt vời ♥️","Yêu cục cưng của bé!!!","Mãi hạnh phúc bên vợ & con nhé♥️"],
+    
+    // Các hình ảnh bay lên khi mở hộp quà (có thể thêm nhiều ảnh)
+    images: ["./assets/user_images_0.jpg","./assets/user_images_1.jpg","./assets/user_images_2.jpg","./assets/user_images_3.jpg","./assets/user_images_4.jpg"],
+    
+    // Màu của trái tim 3D (mã màu Hex)
+    heartColor: "#fd9595"
+};
